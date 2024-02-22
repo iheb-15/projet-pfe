58247de648cd41b1a979ab16bf4ce2e61b7f3b11
iheb nguili <nguiliiheb760@gmail.com>
2024-02-22 11:32:36 +0100

    test navbar et sidebar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,29 +1,20 @@
-
 import "./Navbar.css";
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import Logo from '../media/logo.png';
-
+import Logo1 from '../media/logo1.png';
 const Navbar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
   return (
     <div className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Rec-inov</Link>
+         <img src={Logo1} alt="Logo1" style={{ width: '50px', height: 'auto' }} />  
+         <Link style={{ color:'white' }} to="/" >Rec-inov</Link>
       </div>
+      
       <div className="navbar-links">
         <Link to="/kkk">kkk</Link>
         <Link to="/nnn">nnn</Link>
         <Link to="/sss">sss</Link>
       </div>
-      <div className="navbar-toggle-button" onClick={toggleSidebar}>
-        <i className="fas fa-bars"></i>
-      </div>
+      
     </div>
   );
 };
--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -2,27 +2,34 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { useState } from "react";
 import "./Navbar.css";
-import Logo1 from '../media/logo1.png';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+
 const Sidebar = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  
-    const toggleSidebar = () => {
-      setIsSidebarOpen(!isSidebarOpen);
-    };
+   
+    const [isCollapsed, setIsCollapsed] = useState(false);//
+
+   
+    const handleCollapse = () => {
+      setIsCollapsed(!isCollapsed);};//
   
     return (
-      <div className="sidebar-left">
-        <div className="sidebar-header">
-        <img src={Logo1} alt="Logo1" style={{ width: '50px', height: 'auto' }} />
-          <Link to="/">Rec-inov</Link>
-        </div>
+      <div className={`sidebar-left ${isCollapsed ? 'collapsed' : ''}`}>
+       
         <div className="sidebar-links">
           <Link to="/Oo">Oo</Link>
           <Link to="/cc">Cc</Link>
           <Link to="/st">St</Link>
-         
-        </div>
+          
       </div>
+      <div className="collapse-btn" >
+      <button className="btn" onClick={handleCollapse}  >
+        {isCollapsed ? <FaChevronRight className="icon" /> : <FaChevronLeft className="icon" />}
+      </button>
+      </div>
+     
+        </div>
+         
     );
   };
   
