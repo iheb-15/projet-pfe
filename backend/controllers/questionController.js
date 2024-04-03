004985747bd050416492a676089b7a779e3e4f92
@nguiliiheb760 <nguiliiheb760@gmail.com>
2024-04-03 23:46:58 +0200

    test

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -1,5 +1,5 @@
 const RecinovQuestion = require('../models/Question');
-const Feature = require('../models/Question');
+const Feature = require('../models/Feature');
 const RecinovAnswer = require('../models/answer'); 
 const mongoose = require('mongoose');
 
@@ -65,23 +65,140 @@ exports.filterQuestions = async (req, res) => {
 //     }
 // };
 
-// exports.getAllAnswers = async (req, res) => {
-//     try {
-//       const answers = await RecinovAnswer.find();
-//       res.status(200).json(answers);
-//     } catch (error) {
-//       res.status(500).json({ error: error.message });
-//     }
-//   };
+exports.getAllAnswers = async (req, res) => {
+    try {
+      const answers = await RecinovAnswer.find();
+      res.status(200).json(answers);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
  
 exports.getAllFeatures = async (req, res) => {
     
         try {
             
-            const features = await Feature.find({}, 'skill class -_id');
+            const features = await Feature.find(req.query).select('skill class');
             res.json(features);
           } catch (error) {
             res.status(500).send({ message: error.message });
-          }
-        };
+          } };
+
+          exports.getQuestionWithAnswers = async (req, res) => {
+            
+              try {
+                const lang = req.query.lang; 
+                console.log('Language:', lang);
+            
+                const questions = await RecinovQuestion.find({});
+            
+                if (questions.length === 0) {
+                  return res.status(404).json({ message: 'No questions found' });
+                }
+            
+                const responses = [];
+            
+                for (const question of questions) {
+                  let answerProjection;
+                  if (!lang) {
+                    answerProjection = {
+                      answer_fr: 1,
+                      answer_en: 1,
+                      isCorrect: 1,
+                      _id: 0 
+                    };
+                  } else {
+                    const answerField = lang === 'fr' ? 'answer_fr' : 'answer_en';
+                    answerProjection = {
+                      [answerField]: 1,
+                      isCorrect: 1,
+                      _id: 0 
+                    };
+                  }
+            
+                  const answers = await RecinovAnswer.find({ idQuestion: question._id }, answerProjection);
+            
+                  let questionResponse;
+                  if (!lang) {
+                    questionResponse = {
+                      question_fr: question.question_fr,
+                      question_en: question.question_en,
+                    };
+                  } else {
+                    questionResponse = lang === 'fr' ? question.question_fr : question.question_en;
+                  }
+            
+                  
+                  responses.push({
+                    question: questionResponse,
+                    answers
+                  });
+                }
+            
+                res.json(responses);
+              } catch (error) {
+                console.error('Failed to fetch questions and answers', error);
+                res.status(500).json({ message: 'Server error' });
+              }
+            };
+
+            // exports.getQuestionWithAnswers = async (req, res) => {
+            //   try {
+            //     const questionId = req.params.id;
+            //     const lang = req.query.lang; 
+            //     console.log('Question ID:', questionId, 'Language:', lang);
+            
+               
+            //     const question = await RecinovQuestion.findById(questionId);
+            //     if (!question) {
+            //       return res.status(404).json({ message: 'Question not found' });
+            //     }
+            
+            //     let answerProjection;
+            //     if (!lang) {
+                  
+            //       answerProjection = {
+            //         answer_fr: 1,
+            //         answer_en: 1,
+            //         isCorrect: 1,
+            //         _id: 1
+            //       };
+            //     } else {
+            //       const answerField = lang === 'fr' ? 'answer_fr' : 'answer_en';
+            //       answerProjection = {
+            //         answer: `$${answerField}`,
+            //         isCorrect: 1,
+            //         _id: 1
+            //       };
+            //     }
+            
+            //     const answers = await RecinovAnswer.aggregate([
+            //       { $match: { idQuestion: question._id } },
+            //       {
+            //         $project: answerProjection
+            //       }
+            //     ]);
+            
+                
+            //     let questionResponse;
+            //     if (!lang) {
+            //       questionResponse = {
+            //         question_fr: question.question_fr,
+            //         question_en: question.question_en,
+            //       };
+            //     } else {
+            //       questionResponse = lang === 'fr' ? question.question_fr : question.question_en;
+            //     }
+            
+            //     const response = {
+            //       question: questionResponse,
+            //       answers
+            //     };
+            
+            //     res.json(response);
+            //   } catch (error) {
+            //     console.error('Failed to fetch question and answers', error);
+            //     res.status(500).json({ message: 'Server error' });
+            //   }
+            // };
