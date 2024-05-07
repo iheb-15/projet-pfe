70df25ea745cd6dbd360ad96f0dfab4c8f8452ea
HADIR-SOUF <soufihadir@gmail.com>
2024-05-07 15:27:46 +0100

    update

--- frontend/src/Template.jsx
+++ frontend/src/Template.jsx
@@ -16,28 +16,20 @@ import HomeIcon from '@material-ui/icons/Home';
 import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
 import CreateIcon from '@material-ui/icons/Create';
 import PersonIcon from '@material-ui/icons/Person';
-<<<<<<< HEAD
+
 import { HighlightOutlined, RightOutlined, LeftOutlined } from '@ant-design/icons';
 import TemplateHeader from "./components/TemlplateHeader";
 const { Header, Sider, Content } = Layout;
 const { SubMenu } = Menu;
-=======
+
 import CréerTest from './CréerTest';
 import testFormulaire from './pages/testviaformulaire';
-
->>>>>>> 0c9ab8c139b4b40ffdcd9777a3ebcb14f485a9e4
 
 const Template = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [connected, setConnected] = useState(localStorage.getItem("userrole") || '3');
 
   const history = useHistory();
-<<<<<<< HEAD
-=======
-  const [userName, setUserName] = useState('');
-//changer par Auth user
-const [connected, setConnected] = useState(localStorage.getItem("userrole")?localStorage.getItem("userrole"):'3')
->>>>>>> 0c9ab8c139b4b40ffdcd9777a3ebcb14f485a9e4
 
   useEffect(() => {
     setConnected(localStorage.getItem('userrole'));
@@ -48,7 +40,6 @@ const [connected, setConnected] = useState(localStorage.getItem("userrole")?loca
   };
 
   const handleLogout = () => {
-<<<<<<< HEAD
     localStorage.clear();
     fetch('http://localhost:3002/api/signout', {
       method: 'GET',
@@ -66,70 +57,6 @@ const [connected, setConnected] = useState(localStorage.getItem("userrole")?loca
       });
   };
 
-=======
-      localStorage.clear();
-      history.push('/login'); 
-  };
-//   useEffect(() => {
-//     // Récupérer le token JWT depuis localStorage
-//     const token = localStorage.getItem('token');
-
-//     // Si le token existe, faire une requête au backend pour obtenir les informations de l'utilisateur
-//     if (token) {
-//         fetch('http://localhost:3002/api/name', {
-//             method: 'GET',
-//             headers: {
-//                 'Authorization': `Bearer ${token}`
-//             }
-//         })
-//         .then(response => response.json())
-//         .then(data => {
-//             if (data.name) {
-//                 setUserName(data.name);
-//             } else {
-//                 // Gérer les erreurs, par exemple si le token est invalide
-//                 console.error('Erreur lors de la récupération du nom de l\'utilisateur');
-//             }
-//         })
-//         .catch(error => {
-//             console.error('Erreur lors de la récupération du nom de l\'utilisateur :', error);
-//         });
-//     }
-// }, []);
-// const handlename= () => {
-//   fetch('http://localhost:3002/api/name', {
-//       method: 'GET',
-//       credentials: 'include',
-//   })
-//   .then(response => {
-//       if (response.ok) {
-//           return response.json();
-//       } else {
-//           throw new Error('Erreur lors de la récupération des informations de l\'utilisateur');
-//       }
-//   })
-//   .then(data => {
-//       // Afficher le nom de l'utilisateur récupéré dans votre interface utilisateur
-//       console.log('Nom de l\'utilisateur:', data.name);
-//   })
-//   .catch(error => {
-//       console.error('Erreur:', error.message);
-//   });
-// };
-
-// Appelez cette fonction pour récupérer les informations de l'utilisateur une fois que l'utilisateur est connecté
-// handlename();
-  const userMenu = (
-    <Menu>
-      <Menu.Item key="1">
-        <Link to="/profile" style={{ color: '#000000' }}>Mon profil</Link>
-      </Menu.Item>
-      <Menu.Divider />
-      <Menu.Item key="2" onClick={handleLogout} style={{ color: '#000000' }}>Déconnexion</Menu.Item>
-    </Menu>
-  );
-console.log("connected",connected)
->>>>>>> 0c9ab8c139b4b40ffdcd9777a3ebcb14f485a9e4
   return (
     <Router>
       <Layout style={{ minHeight: '100vh' }}>
@@ -172,29 +99,8 @@ console.log("connected",connected)
           </Button>
         </Sider>
         <Layout className="site-layout">
-<<<<<<< HEAD
           <TemplateHeader />
-          <Content className="site-layout-background" style={{ margin: '24px 16px', padding: 24, minHeight: 280,  }}>
-            <PrivateRoute path="/gest_utilisateur" style={{ background: '#3987ee' }} component={Gest} allowedRoles={['0']} userRole={connected} />
-            <Route path="/ajouter_question" style={{ background: '#3987ee' }} component={AjoutQuestion} />
-            <Route path="/ModifierQuestion/:id" style={{ background: '#3987ee' }} component={Question} />
-            <Route path="/filtrer_Question" style={{ background: '#3987ee' }} component={Filter} />
-            <Route path="/traduire_quest" component={TraduireQuest} />
-            <Route path="/liste_question"  style={{ background: '#3987ee' }} component={ListeQuest} />
-            <Route path="/dashboard"  style={{ background: '#3987ee' }} component={Dashboard} />
-=======
-          <Header className="site-layout-background" style={{ padding: 0, background: '#3987ee' }}>
-            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-              className: 'trigger',
-              onClick: () => setCollapsed(!collapsed),
-            })}
-            <Dropdown overlay={userMenu} trigger={['click']}>
-              <Link to="#" className='ant-dropdown-link' onClick={e => e.preventDefault()} style={{ color: 'white', marginLeft: '88%', cursor: 'pointer', textDecoration: 'none' }}>
-                {user.name} <DownOutlined />
-              </Link>
-            </Dropdown>
-            
-          </Header>
+         
           <Content className="site-layout-background" style={{ margin: '24px 16px', padding: 24, minHeight: 280,  }}>
            <PrivateRoute path="/gest_utilisateur" style={{ background: '#3987ee' }} component={Gest} allowedRoles={['0']} userRole={connected} />
               <Route path="/ajouter_question" style={{ background: '#3987ee' }} component={AjoutQuestion} />
@@ -205,7 +111,6 @@ console.log("connected",connected)
               <Route path="/dashboard"  style={{ background: '#3987ee' }} component={Dashboard} />
               <Route path="/creer_test"  style={{ background: '#3987ee' }} component={CréerTest} />
               <Route path="/test_formulaire"  style={{ background: '#3987ee' }} component={testFormulaire} />
->>>>>>> 0c9ab8c139b4b40ffdcd9777a3ebcb14f485a9e4
           </Content>
         </Layout>
       </Layout>
